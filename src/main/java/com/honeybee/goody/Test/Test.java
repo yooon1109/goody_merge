2bfccd3b3d1c4ec4c2c2ebc700336204ba6b8ea0
yooon <jys_1109@naver.com>
2023-07-26 14:18:17 +0900

    Date타입으로 변경

--- src/main/java/com/honeybee/goody/Test/Test.java
+++ src/main/java/com/honeybee/goody/Test/Test.java
@@ -1,9 +1,15 @@
 package com.honeybee.goody.Test;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.sql.Timestamp;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+
 
 @NoArgsConstructor
 @AllArgsConstructor(staticName = "of")
@@ -12,6 +18,8 @@ import lombok.Setter;
 public class Test {
     private String id;
     private String pw;
-    private long birth;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date birth;
+
 
 }
