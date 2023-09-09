ac1225386893174695e518875df468507b98bea8
devJeenee <eui415566@gmail.com>
2023-09-10 01:42:39 +0900

    삭제

--- src/main/java/com/honeybee/goody/Collection/CollectionGetService.java
+++ src/main/java/com/honeybee/goody/Collection/CollectionGetService.java
@@ -2,11 +2,8 @@ package com.honeybee.goody.Collection;
 
 import com.google.cloud.firestore.*;
 
-import java.sql.Timestamp;
 import java.util.*;
 
-import com.google.firebase.cloud.FirestoreClient;
-import com.honeybee.goody.Post.PreviewDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,20 +17,16 @@ public class CollectionGetService {
     }
 
     public Map<String,Object> getCollectionInfo(String userId, String collectionId) throws Exception {
-        // "Users" 컬렉션에서 "mjc123" userId를 갖는 document를 찾는다
         Query userQuery = firestore.collection("Users").whereEqualTo("userId", userId);
         QuerySnapshot querySnapshot = userQuery.limit(1).get().get();
 
         DocumentSnapshot userDoc = querySnapshot.getDocuments().get(0);
 
-        // userDocument 내부의 "myCollection" 컬렉션에서 "mjc1" collectionId를 갖는 document를 찾는다
         CollectionReference myCollectionRef  = userDoc.getReference().collection("myCollection");
 
-        // "myCollection" 서브 컬렉션 내에서 "mjc1" collectionId를 가진 문서를 찾습니다.
         Query collectionQuery = myCollectionRef.whereEqualTo("collectionId", collectionId);
         QuerySnapshot collectionQuerySnapshot = collectionQuery.get().get();
 
-        // 결과에서 첫 번째 문서를 가져옵니다.
         DocumentSnapshot targetDoc = null;
         if (!collectionQuerySnapshot.isEmpty()) {
             targetDoc = collectionQuerySnapshot.getDocuments().get(0);
