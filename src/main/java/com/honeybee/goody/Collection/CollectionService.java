f244ac5dd71a0972b293ba789e1f842492525e93
devJeenee <eui415566@gmail.com>
2023-09-10 16:44:45 +0900

    사진파일명 만듦...

--- src/main/java/com/honeybee/goody/Collection/CollectionService.java
+++ src/main/java/com/honeybee/goody/Collection/CollectionService.java
@@ -116,7 +116,7 @@ public class CollectionService {
             return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
         }
 
-        List<String> imageUrls = saveImagesToStorage(inputData.getImages());
+        List<String> imageUrls = saveImagesToStorage(newCollectionId, inputData.getImages());
 
         Map<String, Object> data = new HashMap<>();
         data.put("collectionId", newCollectionId);
@@ -138,7 +138,7 @@ public class CollectionService {
         return new ResponseEntity<>("Collection added successfully", HttpStatus.CREATED);
     }
 
-    private List<String> saveImagesToStorage(List<MultipartFile> images) {
+    private List<String> saveImagesToStorage(String collectionId, List<MultipartFile> images) {
         List<String> imageUrls = new ArrayList<>();
         try {
             // Firebase Storage 초기화
@@ -148,7 +148,7 @@ public class CollectionService {
                 String bucketName = FirebaseApp.getInstance().getOptions().getStorageBucket();
                 Bucket bucket = StorageClient.getInstance().bucket(bucketName);//'gs://goody-4b16e.appspot.com'
                 InputStream content = new ByteArrayInputStream(image.getBytes());
-                Blob blob = bucket.create("collections/"+image.getOriginalFilename(),content,image.getContentType());
+                Blob blob = bucket.create("collections/"+collectionId+"-"+image.getOriginalFilename(),content,image.getContentType());
                 imageUrls.add(blob.getName());
             }
         } catch (Exception e) {
